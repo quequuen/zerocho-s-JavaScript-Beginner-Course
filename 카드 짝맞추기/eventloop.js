1b721bf58ca33e56323be1d0f68d58850694b485
quequuen <rkdusl2020@gmail.com>
2025-07-05 20:51:19 +0900

    eventloop.js 수정    1.이벤트루프 예제 동작 순서 추가

--- 카드 짝맞추기/eventloop.js
+++ 카드 짝맞추기/eventloop.js
@@ -15,9 +15,9 @@ setTimeout(() => {
   console.log("b");
 });
 
-/*호출스택: anonymous 추가 - > setTimeout 추가 -> setTimeout 제거 
-백그라운드: 타이머1-0초, 타이머2-0초
+/*호출스택: anonymous 추가 - > setTimeout 추가 -> setTimeout 제거 ->console.log(a) 호출 -> aaa ->setTimeout->console.log(c)-> aaa 종료-> 타이머2-0초, aaa, setTimeout-> console.log(c)-> console.log(b)-> console.log(d) -> console.log(d)
+백그라운드: 타이머1-0초, 타이머2-0초, 타이머3-0초, 타이머3-0초
 태스크큐: 
-콘솔:
+콘솔: a,c,c,b,d,d
 
 */
