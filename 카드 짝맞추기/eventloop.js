b6af24362eb8169da33a1fda92a6f823824240ee
quequuen <rkdusl2020@gmail.com>
2025-07-07 18:37:41 +0900

    Fix: concentration.html, eventloop.js 수정
    - eventloop.js 부연 설명 추가
    - concentration.html prompt 사용으로 total(총 카드 개수) 사용자가 결정, startTime, endTime 추가로 게임 종료시, 진행된 시간 표시

--- 카드 짝맞추기/eventloop.js
+++ 카드 짝맞추기/eventloop.js
@@ -20,4 +20,9 @@ setTimeout(() => {
 태스크큐: 
 콘솔: a,c,c,b,d,d
 
+**호출스택이 종료된 후** 백그라운드에 쌓인 타이머들을 순서대로 동작 후 이벤트 루프가 하나씩 태스트큐에 옮겨주고 그 태스크 큐에 있는 것들을 호출 스택에서 끌어다 쓴다.
+(=백그라운드에 쌓인 애들은 호출스택이 종료한 후 같이 순차적으로 실행되어야 한다.)
+그래서 aaa() 안의 console.log('d')가 나중에 나오는 것!
+(11-7강 내용 자주 볼 것)
+http://latentflip.com/loupe/?code=ZnVuY3Rpb24gYWFhKCkgewogIHNldFRpbWVvdXQoKCkgPT4gewogICAgY29uc29sZS5sb2coImQiKTsKICB9LCAwKTsKICBjb25zb2xlLmxvZygiYyIpOwp9CgpzZXRUaW1lb3V0KCgpID0%2BIHsKICBjb25zb2xlLmxvZygiYSIpOwogIGFhYSgpOwp9LCAwKTsKCnNldFRpbWVvdXQoKCkgPT4gewogIGFhYSgpOwogIGNvbnNvbGUubG9nKCJiIik7Cn0pOwo%3D!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
 */
